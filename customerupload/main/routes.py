a328ab5d7c512686fc7b5a8695bcacc6801347a3
James Fallisgaard <james.fgaard@gmail.com>
2019-02-12 17:44:16 -0500

    working end to end

--- customerupload/main/routes.py
+++ customerupload/main/routes.py
@@ -1,16 +1,26 @@
-from flask import render_template, request, Blueprint
+import os
+import time
+from flask import render_template, request, redirect, Blueprint
 from customerupload.process_input import ProcessInput
+from werkzeug.utils import secure_filename
 
 main = Blueprint('main', __name__)
 
-
 @main.route("/")
-@main.route("/home")
 def home():
     return render_template('home.html')
 
-@main.route("/process")
-def process():
-    processor = ProcessInput(ProcessInput.UNPROCESSED_PATH + "input-sample.tsv")
-    processed_rows = processor.execute()
-    return render_template('complete.html', processed_rows=processed_rows)
+@main.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+        file = request.files['file']
+        start_time = time
+        file_path = os.path.join(ProcessInput.UNPROCESSED_PATH,
+                                 start_time.strftime("%Y%m%d-%H%M%S") + '.tsv')
+        if not os.path.exists(ProcessInput.UNPROCESSED_PATH):
+            os.makedirs(ProcessInput.UNPROCESSED_PATH)
+        file.save(file_path)
+        
+        processor = ProcessInput(file_path, start_time)
+        processed_rows = processor.execute()
+        return render_template('complete.html', processed_rows=processed_rows)
